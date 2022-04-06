36582adb905524c7a1af066b7150133e4da362bf
Napat48 <pondnapatj@gmail.com>
2022-04-06 11:49:42 +0700

    test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import fastapi
 
 
 
-app = FastAPI(__name__)
+app = FastAPI()
 
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 
@@ -26,8 +26,6 @@ def read_item(course: str,name:str,cls :str , num:str , email :str):
 
             "append":sheet.append_row([course,name,cls,num,email])}
 
-if __name__ == '__main__':
-    app.run()
 
 
 
